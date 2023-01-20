32b94a78f7fb35862f8237c9424c95ae805274dd
tomyyyD <thomas5d@comcast.net>
2023-01-20 11:59:17 -0500

    Revert "started threading in cubesate"
    This reverts commit a963cc8f405ad10523a42186c99f55f2a2576528.

--- fakecubesat.py
+++ fakecubesat.py
@@ -1,5 +1,5 @@
 import time
-import threading
+import os
 from random import randint
 from multiprocessing import shared_memory
 
@@ -7,41 +7,24 @@ control_data = 0
 UP_FIFO = "/tmp/satuplink"
 DOWN_FIFO = "/tmp/satdownlink"
 
-# Add threading to the random reads
-
-
-def read_file():
-    # read from the Julia data to update data
-    # print("Starting data read...")
-    try:
-        read_file = open(DOWN_FIFO, "r")
-        # print("file opened")
-        print(read_file.read())
-
-    except Exception as e:
-        print(f"Failed to read data: {e}")
-
-
 while True:
     # send control data to Julia sim
-    # print("opening uplink...")
+    print("opening uplink...")
     try:
         write_file = open(UP_FIFO, "w")
-        # print("FIFO opened")
+        print("FIFO opened")
         write_file.write(time.time(), control_data)
-        # print("written to file")
+        print("written to file")
     except Exception as e:
-        print(f"Failed to Open satuplink: {e}")
+        print(e)
 
     if randint > 0.5:
         # read from the Julia data to update data
-        # print("Starting data read...")
         try:
             read_file = open(DOWN_FIFO, "r")
-            # print("file opened")
             print(read_file.read())
 
         except Exception as e:
-            print(f"Failed to read data: {e}")
+            print(e)
 
     time.sleep(0.1)
