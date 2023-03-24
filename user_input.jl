3ec8f60f67bd458514d93be5e24e245c1b828740
tomyyyD <thomas5d@comcast.net>
2023-03-23 20:26:05 -0400

    adding some defaults

--- user_input.jl
+++ user_input.jl
@@ -1,2 +1,26 @@
 # where the user will input their own functions to be used
 # and where some basic defaults will be
+
+using GNCTestServer
+
+struct trueState
+    state::Any
+    params::Any
+end
+
+struct PycubedMiniState
+    position :: Vector{Float64}
+    velocity :: Vector{Float64}
+end
+
+struct PycubedMini <: trueState
+    state::PycubedMiniState
+    params::Any
+ends
+
+function measurement(x::trueState)
+    return x.state.position
+end
+
+GNCTestServer.simulate()
+
