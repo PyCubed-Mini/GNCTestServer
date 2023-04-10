4e5c86a407afee4caf50ab9b527e37d851d7071c
thetazero <lesha.seletskiy@gmail.com>
2023-04-10 14:00:06 -0400

    Update docs build script

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
 # Inside make.jl
 push!(LOAD_PATH, "../src/")
-using GNCTestServer
+using SatellitePlayground
 using Documenter
 makedocs(
-    sitename="GNCTestServer.jl",
-    modules=[GNCTestServer],
+    sitename="SatellitePlayground.jl",
+    modules=[SatellitePlayground],
     pages=[
         "Home" => "index.md"
     ])
 deploydocs(;
-    repo="https://github.com/PyCubed-Mini/GNCTestServer"
+    repo="https://github.com/PyGNC/SatellitePlayground.jl"
 )
