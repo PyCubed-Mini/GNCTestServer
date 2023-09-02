af648f642bf14b581c2d2d835f73af32e550cf3a
thetazero <lesha.seletskiy@gmail.com>
2023-09-02 15:48:12 -0400

    working unit tests, broken ci

--- test/dynamics.jl
+++ test/dynamics.jl
@@ -1,42 +1,19 @@
 begin
-    using Test
     using LinearAlgebra
     using SatelliteDynamics
-    include("../src/SatellitePlayground.jl")
-    SP = SatellitePlayground
-end
-
-begin
-    x = SP.RBState([6.7751363e6
-        0.0
-        0.0
-        0.0
-        8155.162619089651
-        0.0
-        -0.9442165245970416
-        -0.11229349782424022
-        0.28649111413323164
-        0.117337830843173
-        0.013209720675656584
-        0.36976717864576536
-        -0.548490925366443]
-    )
-    model = SP.default_model
-    env = SP.default_environment
-    env = SP.state_view_environment(x, env)
 end
 
 @testset "Airspeed from state" begin
     expected = [0.0, 8649.213359431995, 0.0]
     # 0.000017 seconds (7 allocations: 304 bytes)
-    @time got = SP.airspeed_from_state(x)
+    @time got = SP.airspeed_from_state(default_data.state)
     @test got ≈ expected
 end
 
 @testset "Drag acceleration torque" begin
     a_expected = [1.9645203449589757e-6, -6.508239236619761e-6, 9.950592930420614e-7]
     τ_expected= [1.3610453269277392e-9, 1.0339757656912846e-25, -2.6870772966569952e-9]
-    @time a, τ = SP.drag_acceleration_torque(x, model, env)
+    @time a, τ = SP.drag_acceleration_torque(default_data.state, default_data.model, default_data.env)
     # 0.000035 seconds (111 allocations: 6.891 KiB)
     @test a ≈ a_expected
     @test τ ≈ τ_expected
@@ -44,7 +21,7 @@ end
 
 @testset "Cartesian acceleration & torque" begin
     u = [0.0, 0.0, 0.0]
-    @time a, τ = SP.cartesian_acceleration_torque(x, u, model, env)
+    @time a, τ = SP.cartesian_acceleration_torque(default_data.state, u, default_data.model, default_data.env)
     # 0.000059 seconds (383 allocations: 25.406 KiB)
     a_expected = [-8.696182355824059, -4.582971748165271e-5, -0.00010804973126869058]
     τ_expected = [-2.0364643283794252e-9, 2.6084352385283605e-9, -6.775772747899396e-9]
@@ -52,7 +29,7 @@ end
     @test τ ≈ τ_expected
 
     u = [4.0, 3.0, 2.0]
-    @time a, τ = SP.cartesian_acceleration_torque(x, u, model, env)
+    @time a, τ = SP.cartesian_acceleration_torque(default_data.state, u, default_data.model, default_data.env)
     # 0.000127 seconds (383 allocations: 25.406 KiB)
     a_expected = [-8.696182355824059, -4.582971748165271e-5, -0.00010804973126869058]
     τ_expected= [1.8457691095622258e-7, -1.3719520438198725e-7, -5.3585508411985804e-8]
--- test/fast_auto_test.jl
+++ test/fast_auto_test.jl
@@ -1,7 +1,6 @@
-using Test
-using LinearAlgebra
-include("../src/SatellitePlayground.jl")
-SP = SatellitePlayground
+begin
+    using LinearAlgebra
+end
 
 function no_control(measurement)
     return zero(SP.Control)
@@ -11,19 +10,19 @@ end
     function log_step(hist, state)
         push!(hist, state)
     end
-    (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0)
+    (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0, initial_condition=default_data.state)
     @test length(data) == 100
     @test length(time) == 100
     @test data[1] isa SP.RBState
 
-    (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0)
+    (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0, initial_condition=default_data.state)
     data = SP.vec_to_mat(data)
     @test size(data) == (100, 13)
     @test length(time) == 100
 end
 
 @testset "default logging" begin
-    (data, time) = SP.simulate(no_control, max_iterations=100)
+    (data, time) = SP.simulate(no_control, max_iterations=100, initial_condition=default_data.state)
     @test length(data) == 100
     @test length(time) == 100
 end
@@ -32,7 +31,7 @@ end
    function log_step(hist, state) 
     push!(hist, norm(state.angular_velocity))
    end
-   (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0)
+   (data, time) = SP.simulate(no_control, max_iterations=100, log_step=log_step, dt=10.0, initial_condition=default_data.state)
    @test length(data) == 100
    @test length(time) == 100
 end
